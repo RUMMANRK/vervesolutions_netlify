06f3308ba31061c392ab822c4e0674b45756d503
RUMMANRK <60428085+RUMMANRK@users.noreply.github.com>
2023-05-23 15:22:28 +0600

    Fixed Navbar, hover state and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,16 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    colors: {
-      'mainbg': "#D1D2D4",
-      'mainblack': "#101010",
-      'mainff': "#FFFFFF",
-    },
 
-    extend: {}
+
+    extend: {
+      colors: {
+        'mainbg': "#D1D2D4",
+        'mainblack': "#101010",
+        'mainff': "#FFFFFF",
+        'buttonHover': "#404040"
+      },
+    }
   },
   plugins: []
 };
